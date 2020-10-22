f80e900ac463b7e3db9ca3f58ee10b58573704de
mmalaczek <miroslaw.malaczek@empik.com>
2020-10-22 18:43:31 +0200

    Entity Comment

---
+++ api/src/Entity/Comment.php
@@ -0,0 +1,75 @@
+<?php
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="datetime")
+     * @var DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+}
